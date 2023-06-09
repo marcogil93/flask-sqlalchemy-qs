341113901ee70c4f31acad9ea56e67527621db81
LuisCastilloH <andres.castillo.hdez@gmail.com>
2023-06-09 16:06:58 -0600

    Create constants.py

---
+++ flask_sqlalchemy_qs/qs_parser/constants.py
@@ -0,0 +1,26 @@
+"""
+This file is to integrate constants
+"""
+
+CONDITIONS = {
+    "eq": "__eq__",
+    "ne": "__ne__",
+    "lt": "__lt__",
+    "lte": "__le__",
+    "gt": "__gt__",
+    "gte": "__ge__",
+    "in": "in_",
+    "nin": "notin_",
+    "contains": "contains",
+    "ncontains": "ncontains",
+    "icontains": "icontains",
+    "like": "like",
+    "ilike": "ilike",
+    "not_like": "not_like",
+    "not_ilike": "not_ilike",
+    "nicontains": "nicontains",
+    "startswith": "startswith",
+    "istartswith": "istartswith",
+    "endswith": "endswith",
+    "iendswith": "iendswith"
+}
